1ee752c36da1ae77c56e6bbecbf7984c77258f20
Roy Wei <roywjy@gmail.com>
2022-06-11 15:47:34 +1000

    Create finishing mechanism in setup

--- lib/toy_robot/setup.rb
+++ lib/toy_robot/setup.rb
@@ -1,14 +1,33 @@
 class Setup
 
-  table = Table.new
-  robot_simulator = Simulator.new(table)
-
-  puts "Enter a command or type EXIT to quit:"
-
-  command = gets.chomp
-  while command !~ /EXIT/
-    output = robot_simulator.execute(command)
-    puts output if output
-    command = gets.chomp
+  def initialize
+    table = Table.new(5, 5)
+    @simulator = Simulator.new(table)
   end
+
+  def execute
+    puts "Enter a command or type EXIT to quit:"
+
+    command = gets.chomp
+    while command.downcase !~ /exit/
+      output = @simulator.execute(command)
+      puts output if output
+
+      # To stop looping
+      command = gets.chomp
+    end
+
+    finish_game
+  rescue Interrupt
+    finish_game
+  end
+
+  private
+
+    def finish_game
+      system "clear" or system "cls"
+      puts "Well, that's it!\n\n"
+      exit
+    end
+
 end
